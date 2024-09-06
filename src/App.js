1e39e134e5e1eebebf77160261390b75ffc69da2
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-09-06 11:46:32 +0300

    Create first local project React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { About } from './components/About';
+import { Home } from './components/Home';
+import { Menu } from './components/Menu';
+import { Privacy } from './components/Privacy';
+import {  Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <h1>Hello React</h1>  
+        <Menu/>      
       </header>
+      <main>               
+          {/* <Routes>
+                <Route path="/" element={<Home />}>
+                <Route path="/about" element={<About />} />
+                <Route path="/privacy" element={<Privacy />} />              
+              </Route>
+         </Routes>    */}
+         <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/privacy" element={<Privacy />} />
+           </Routes>       
+      </main>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+export  function Home()
+{
+    return(
+        <>
+            <h2>Welcome</h2>
+            <p>This is Home page</p>
+        </>
+    );
+}
